88505888c25aa7d8fe92bae4a1c03787817de2f3
Adi0138 <42856438+Adi0138@users.noreply.github.com>
2021-05-08 21:02:16 +0700

    Add files via upload

---
+++ soal3/soal3.c
@@ -0,0 +1,120 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/stat.h> 
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<dirent.h>
+#define MAX 1000
+
+char cwd[MAX];
+
+int isFileExist(char *path) 
+{
+    struct stat path_stat;
+    stat(path, &path_stat);
+    return S_ISREG(path_stat.st_mode);
+}
+
+char *get_filename_ext(char *filename) {
+    char *dot = strrchr(filename, '.');
+    if(!dot || dot == filename) return "";
+    return dot + 1;
+}
+
+void* playandcount(void *arg){ 
+
+    char *fullname = (void *) arg;
+   
+    int file_exist = isFileExist(fullname);
+    char source[MAX], dest[MAX],path[MAX];
+    if(file_exist){
+        
+        char* ext = get_filename_ext(fullname);
+        
+        char *abspath = (void *) arg;
+        if(strcmp(ext,"")!= 0){
+            strcpy(source,abspath);
+            strcpy(dest, cwd);
+            strcat(dest, "/");
+            char* file_path= strrchr(fullname, '/');
+            strcpy(path,file_path);
+            for(int i = 0; ext[i];i++){
+                ext[i]= tolower(ext[i]);
+            }
+            mkdir(ext, 0777);
+            strcat(dest, (ext));
+            strcat(dest,path);
+            rename(source,dest);
+        }
+        else{ 
+            printf("2");
+            mkdir("Unknown",0777);
+            strcpy(dest, cwd);
+            strcat(dest, "/");
+            strcat(dest,"Unknown"); 
+            char* file_path= strrchr(fullname, '/');
+            strcat(dest,file_path);
+            rename(abspath,dest);
+        }
+    }
+    
+    pthread_exit(0);
+}
+
+int main(int argc, char** argv) {
+    getcwd(cwd,sizeof(cwd));
+    pthread_t tid[MAX];
+    int i = 0; 
+    int test;   
+    if(strcmp(argv[1], "-f") == 0){
+        for(test = 2; test < argc; test++){
+            pthread_create(&(tid[i]),NULL,playandcount,argv[test]);
+            pthread_join(tid[i],NULL);
+            i++;
+        }
+    }
+    else if(strcmp(argv[1],"*") == 0){
+        DIR *d;
+        struct dirent *dir;
+        d = opendir(".");
+        if (d) {
+            while ((dir = readdir(d)) != NULL) {
+                char filePath[1000];
+                if(isFileExist(dir->d_name)){
+                    strcpy(filePath, cwd);
+                    strcat(filePath, "/");
+                    strcat(filePath, dir->d_name);
+                    printf("%s\n",filePath);
+                    pthread_create(&(tid[i]),NULL,playandcount,filePath); 
+                    pthread_join(tid[i],NULL);
+                    i++;
+                }
+            }
+            closedir(d);
+        }
+    }
+    else if(strcmp(argv[1],"-d") == 0){
+        DIR *d;
+        struct dirent *dir;
+        d = opendir(argv[2]);
+        if (d) {
+            while ((dir = readdir(d)) != NULL) {
+                char filePath[1000];
+                strcpy(filePath,argv[2]);
+                strcat(filePath, "/");
+                strcat(filePath,dir->d_name);
+                if(isFileExist(filePath)){
+                    pthread_create(&(tid[i]),NULL,playandcount,filePath); 
+                    pthread_join(tid[i],NULL);
+                    i++;
+                }
+            }
+            closedir(d);
+        }
+        else printf("No Dir\n");
+    }
+}
