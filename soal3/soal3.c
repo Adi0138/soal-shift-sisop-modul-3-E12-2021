470dbbf08e292861c88b9d2146f394409a698a52
Adi0138 <42856438+Adi0138@users.noreply.github.com>
2021-05-23 19:13:43 +0700

    Add files via upload

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -12,14 +12,14 @@
 
 char cwd[MAX];
 
-int isFileExist(char *path) 
+int isFileExist(char *path) //berguna untuk mengecek apakah path yang sedang kita tuju file/bukan
 {
     struct stat path_stat;
     stat(path, &path_stat);
     return S_ISREG(path_stat.st_mode);
 }
 
-char *get_filename_ext(char *filename) {
+char *get_filename_ext(char *filename) { //berguna untuk mengambil suatu ekstensi dari suatu file tanpa menyertakan '.'
     char *dot = strrchr(filename, '.');
     if(!dot || dot == filename) return "";
     return dot + 1;
@@ -28,13 +28,13 @@ char *get_filename_ext(char *filename) {
 void* playandcount(void *arg){ 
 
     char *fullname = (void *) arg;
-   
+    //cek exist ga
     int file_exist = isFileExist(fullname);
     char source[MAX], dest[MAX],path[MAX];
     if(file_exist){
-        
+        //cari extension
         char* ext = get_filename_ext(fullname);
-        
+        //jadi huruf kecil
         char *abspath = (void *) arg;
         if(strcmp(ext,"")!= 0){
             strcpy(source,abspath);
@@ -65,18 +65,23 @@ void* playandcount(void *arg){
     pthread_exit(0);
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv) { 
+    //getcwd(cwd,sizeof(cwd)); digunakan untuk mendapatkan current working directory pthread_t tid[MAX]; inisialisasi awal thread
     getcwd(cwd,sizeof(cwd));
     pthread_t tid[MAX];
     int i = 0; 
     int test;   
     if(strcmp(argv[1], "-f") == 0){
-        for(test = 2; test < argc; test++){
+        for(test = 2; test < argc; test++){ //Cek apakah argumen yang diberikan adalah -f jika true maka lanjut dimana user bisa menambahkan argumen file yang ingin dikategorikan sesuka hati user.
             pthread_create(&(tid[i]),NULL,playandcount,argv[test]);
             pthread_join(tid[i],NULL);
             i++;
         }
     }
+    /*Fungsi opendir untuk menjadikan direktori file dijalankan sebagai pusat, 
+    lalu readdir untuk membuka direktori sekaligus mengecek apakah direktori kosong atau tidak. 
+    Nantinya akan dibuat thread yang meng-passing filePath yang merupakan path dari file c ini dijalankan. 
+    disini filePath hanya akan mengecek file, karena sudah diperiksa oleh fungsi isFileExist.*/
     else if(strcmp(argv[1],"*") == 0){
         DIR *d;
         struct dirent *dir;
@@ -97,6 +102,12 @@ int main(int argc, char** argv) {
             closedir(d);
         }
     }
+
+    /*mengecek dengan opendir apakah direktori yang user masukkan ada/exist
+    Ketika direktori berhasil dibuka, akan melakukan iterasi pada seluruh file ataupun folder dalam suatu direktori.
+    Lalu direktori exist, maka akan membuat thread yang mempassing path dari suatu file. 
+    Disini yang akan diperiksa cuma file saja sebab telah diperiksa oleh fungsi isFileExist.
+    */
     else if(strcmp(argv[1],"-d") == 0){
         DIR *d;
         struct dirent *dir;
@@ -111,10 +122,11 @@ int main(int argc, char** argv) {
                     pthread_create(&(tid[i]),NULL,playandcount,filePath); 
                     pthread_join(tid[i],NULL);
                     i++;
+                    printf("Direktori sukses disimpan!\n");
                 }
             }
             closedir(d);
         }
-        else printf("No Dir\n");
+        else printf("Yah, gagal disimpan :(\n");
     }
 }
