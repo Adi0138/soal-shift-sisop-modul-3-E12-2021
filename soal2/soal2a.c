7ede595b1cd02996213b4e589b76d7c70575b510
DaffaTristan <72509035+DaffaTristan@users.noreply.github.com>
2021-05-23 19:59:36 +0700

    Update soal2a.c

--- soal2/soal2a.c
+++ soal2/soal2a.c
@@ -1,32 +1,77 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <pthread.h>
 
-int main() {
-  int m1[10][10], m2[10][10], m3[10][10];
-	memset(m1, 0, sizeof m1);
-	memset(m2, 0, sizeof m2);
-	memset(m3, 0, sizeof m3);
+int (*value)[6];
+int first[4][3], second[3][6];
 
-	printf("Enter Matrix 4x3 :\n");
-	for(int i=0 ; i<4 ; i++) {
-		for(int j=0 ; j<3 ; j++) scanf("%d", &m1[i][j]);
-	}
-	printf("\nEnter Matrix 3x6 :\n");
-	for(int i=0 ; i<3 ; i++) {
-                for(int j=0 ; j<6 ; j++) scanf("%d", &m2[i][j]);
-        }
-	for(int i=0 ; i<4 ; i++) {
-                for(int j=0 ; j<6 ; j++) {
-			m3[i][j] = 0;
-			for(int k=0 ; k<6 ; k++) m3[i][j] += (m1[i][k] * m2[k][j]);
-		}
-        }
-	printf("\nMultiplication Result Matrix 4x6:\n");
-	for(int i=0 ; i<4 ; i++) {
-                for(int j=0 ; j<6 ; j++) {
-			printf("%d\t", m3[i][j]);
-		}
-		printf("\n");
-        }
-	return 0;
+void prtScreen() {
+    	printf("\nMultiplication Result Matrix 4x6:\n");
+    	for(int i=0; i<4; i++) {
+        	for(int j=0; j<6; j++) {
+            		printf("%d\t", value[i][j]);
+        	}
+        	printf("\n");
+    	}
+   	printf("\n");
+    	sleep(5);
+}
+
+void *multiplyMatrices(void *arg) {
+    	int sum = 0;
+
+    	for(int i=0; i<4; i++) {
+        	for(int j=0; j<6; j++) {
+            	for(int k=0; k<3; k++) {
+                	sum += first[i][k] * second[k][j];
+            	}
+            	value[i][j] = sum;
+            	sum = 0;
+        	}
+    	}
+}
+
+void inputMatriksB() {
+    	for(int i=0; i<3; i++) {
+        	for(int j=0; j<6; j++) {
+            		scanf("%d", &second[i][j]);
+        	}
+    	}
+}
+
+void inputMatriksA() {
+    	for(int i=0; i<4; i++) {
+        	for(int j=0; j<3; j++) {
+            		scanf("%d", &first[i][j]);
+        	}
+    	}
+}
+
+int main(void) {
+
+    	key_t key = 12345;
+
+    	int shmid = shmget(key, sizeof(int[4][6]), IPC_CREAT | 0666);
+    	value = shmat(shmid, NULL, 0);
+
+	printf("Enter Matrix 4x3 ;\n");
+    	inputMatriksA();
+	printf("Enter Matrix 3x6 ;\n");
+    	inputMatriksB();
+
+    	pthread_t tid[4];
+
+    	for(int i=0; i<4; i++) {
+        	pthread_create(&tid[i], NULL, multiplyMatrices, NULL);
+    	}
+    	for(int i=0; i<4; i++) {
+        	pthread_join(tid[i], NULL);
+    	}
+
+    	prtScreen();
+
+    	shmdt(value);
 }
